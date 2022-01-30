01d82218e5d841e8144089b66123b4c9c2bf996c
xiaolan <13262631372@163.com>
2022-01-30 17:43:07 +0800

    feat(all project): 添加样式组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React,{  FC } from "react";
+import React, { FC } from "react";
+import styled from "styled-components";
 
-export const App:FC = () =>{
-   return (
+
+export const App: FC = () => {
+  return (
+    <Container name="xiaolan">
       <div>react</div>
+      <Wrpper>
+        2222
+      </Wrpper>
+    </Container>
 
-   );
+
+  );
 }
+const Container = styled.div<{name?:string}>`
+  width:${props => props.name ? '200px' : '100px'};
+  height:200px;
+  background:red;
+
+`
+const Wrpper = styled(Container)`
+  color:black;
+  font-weight:bold;
+  background:#78e710;
+`
